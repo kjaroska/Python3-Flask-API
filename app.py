db25301b6f073cabe437e71f3a7aca1cd02b44fe
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-09-11 20:51:39 +0200

    Add stubs for API Endpoints

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
 from flask import Flask
 
 app = Flask(__name__)
+stores = [
+    {
+        "name": "Store 1",
+        "items": [
+            {
+                "name": "Item 1",
+                "price": 15.99
+            }
+        ]
+    }
+]
 
 
 @app.route('/')
-def hello_world():
+def home():
     return 'Hello World!'
 
 
+@app.route('/store', methods=["GET"])
+def get_stores():
+    pass
+
+
+@app.route('/store/<string:name>', methods=["GET"])
+def get_store(name):
+    pass
+
+
+@app.route('/store', methods=["POST"])
+def create_store():
+    pass
+
+
+@app.route('/store/<string:name>/item', methods=["GET"])
+def get_item_in_store(name):
+    pass
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
