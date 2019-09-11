4b433cf5b92bdbc6f45c3f54d1cb8066f4d72ac6
Krzysztof D. Jaroska <krzysztof.d.jaroska@pl.abb.com>
2019-09-11 21:04:54 +0200

    Implement basic Endpoints for API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 stores = [
@@ -21,22 +21,53 @@ def home():
 
 @app.route('/store', methods=["GET"])
 def get_stores():
-    pass
+    return jsonify({"stores": stores})
 
 
 @app.route('/store/<string:name>', methods=["GET"])
 def get_store(name):
-    pass
+    for store in stores:
+        if store["name"] == name:
+            return jsonify(store)
+
+        return jsonify({"message": "Store not found"})
 
 
 @app.route('/store', methods=["POST"])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        "name": request_data["name"],
+        "items": []
+    }
+
+    stores.append(new_store)
+    return jsonify(new_store)
 
 
 @app.route('/store/<string:name>/item', methods=["GET"])
 def get_item_in_store(name):
-    pass
+    for store in stores:
+        if store["name"] == name:
+            return jsonify({"items": store["items"]})
+
+        return jsonify({"message": "Store not found"})
+
+
+@app.route('/store/<string:name>/item', methods=["POST"])
+def create_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            request_data = request.get_json()
+            new_item = {
+                "name": request_data["name"],
+                "price": request_data["price"]
+            }
+            
+            store["items"].append(new_item)
+            return jsonify({"message": "Item added successfully"}, new_item)
+
+        return jsonify({"message": "Store not found"})
 
 
 if __name__ == '__main__':
