a8224ad804438c66778d5f212759730d2663fe69
Krzysztof D. Jaroska <kjaroska@gmail.com>
2019-09-12 20:48:35 +0200

    Add item put endpoint

--- app.py
+++ app.py
@@ -35,6 +35,15 @@ class Item(Resource):
         items = list(filter(lambda x: x["name"] != name, items))
         return {"message": "Item deleted"}, 204
 
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x["name"] == name, items), None)
+        if item is None:
+            item = {"name": name, "price": data["price"]}
+            items.append(item)
+        else:
+            return item, 201
+
 
 class ItemList(Resource):
     def get(self):
