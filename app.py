88b156262f468fc5f650e79a99cdb01056186606
Krzysztof D. Jaroska <kjaroska@gmail.com>
2019-09-17 15:46:26 +0200

    Extract reqparser into Item class level

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ items = [{"name": "chair", "price": 15.88}]
 
 
 class Item(Resource):
+    parser.reqparse.RequestParser()
+    parser.add_argument("price", type=float, required=True, help="This field cannot be left blank!")
+
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda i: i["name"] == name, items), None)
@@ -25,7 +29,7 @@ class Item(Resource):
         if item is not None:
             return {"message": "An item with name '{}' already exists".format(name)}, 400
 
-        data = request.get_json(force=True)
+        data = Item.parser.parse_args()
         new_item = {"name": name, "price": data["price"]}
         items.append(new_item)
         return new_item, 201
@@ -36,9 +40,7 @@ class Item(Resource):
         return {"message": "Item deleted"}, 204
 
     def put(self, name):
-        parser.reqparse.RequestParser()
-        parser.add_argument("price", type=float, required=True, help="This field cannot be left blank!")
-        data = parser.parse_args()
+        data = Item.parser.parse_args()
 
         item = next(filter(lambda x: x["name"] == name, items), None)
         if item is None:
