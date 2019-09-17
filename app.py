30928846a6e273a7327117ed8cbc4cdaab30902f
Krzysztof D. Jaroska <kjaroska@gmail.com>
2019-09-17 15:42:11 +0200

    Import reqparse, implement reqparse in PUT endpoint

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -36,7 +36,10 @@ class Item(Resource):
         return {"message": "Item deleted"}, 204
 
     def put(self, name):
-        data = request.get_json()
+        parser.reqparse.RequestParser()
+        parser.add_argument("price", type=float, required=True, help="This field cannot be left blank!")
+        data = parser.parse_args()
+
         item = next(filter(lambda x: x["name"] == name, items), None)
         if item is None:
             item = {"name": name, "price": data["price"]}
