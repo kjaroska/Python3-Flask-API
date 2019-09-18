df0a391aec6841d8acef690d9151fde334ac7799
Krzysztof D. Jaroska <kjaroska@gmail.com>
2019-09-18 17:12:58 +0200

    Remove Item and ItemList from app.py

--- app.py
+++ app.py
@@ -1,69 +1,24 @@
-from flask import Flask, request, render_template
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
 
 from security import authenticate, identity
 from user_register import UserRegister
+from item import Item, ItemList
 
 app = Flask(__name__)
-app.debug = True
 app.secret_key = "secret_key"
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
-items = [{"name": "chair", "price": 15.88}]
-
-
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument("price", type=float, required=True, help="This field cannot be left blank!")
-
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda i: i["name"] == name, items), None)
-        return {"item": item}, 200 if item is not None else 404
-
-    def post(self, name):
-        item = next(filter(lambda i: i["name"] == name, items), None)
-        if item is not None:
-            return {"message": "An item with name '{}' already exists".format(name)}, 400
-
-        data = Item.parser.parse_args()
-        new_item = {"name": name, "price": data["price"]}
-        items.append(new_item)
-        return new_item, 201
-
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x["name"] != name, items))
-        return {"message": "Item deleted"}, 204
-
-    def put(self, name):
-        data = Item.parser.parse_args()
-
-        item = next(filter(lambda x: x["name"] == name, items), None)
-        if item is None:
-            item = {"name": name, "price": data["price"]}
-            items.append(item)
-        else:
-            return item, 201
-
-
-class ItemList(Resource):
-    def get(self):
-        return {"item": items}
-
-
 api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemList, "/items")
 api.add_resource(UserRegister, "/register")
-
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
-
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
