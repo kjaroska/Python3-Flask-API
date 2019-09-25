55e87786753e9e8db4570a4b3d86433388c88d36
Krzysztof D. Jaroska <kjaroska@gmail.com>
2019-09-25 16:45:31 +0200

    Add Blacklist for revoked users

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, render_template, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from blacklist import BLACKLIST
 
 from application._resources.user_register import UserRegister, User, UserLogin, TokenRefresh
 from application._resources.item import Item, ItemList
@@ -13,6 +14,8 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///application/_static/data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True # Flask extensions can return their internal exceptions and custom errors
+app.config["JWT_BLACKLIST_ENABLED"] = True
+app.config["JWT_BLACKLIST_TOKEN_CHECKS"] = ["access", "refresh"]
 app.secret_key = "secret_key"
 api = Api(app)
 
@@ -24,6 +27,10 @@ def add_claims_to_jwt(identity): #identity => user.id
         return {"is_admin": True}
     
     return {"is_admin": False}
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token["identity"] in BLACKLIST
 
 @jwt.expired_token_loader # if user token is expired during request
 def expired_token_callback():
